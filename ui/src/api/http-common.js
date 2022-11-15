a7a54f354a2859e5b38d3c40fe4cdb39a2f8859e
stefanofiorenzaknits <stefano.fiorenza.knits@gmail.com>
2022-11-15 14:29:35 +0200

    Restored headers for axios

--- ui/src/api/http-common.js
+++ ui/src/api/http-common.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const httpCommon = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  // headers: {
-  //   "Content-type": "application/json",
-  //   Accept: "application/json",
-  // },
+  headers: {
+    "Content-type": "application/json",
+    Accept: "application/json",
+  },
 });
